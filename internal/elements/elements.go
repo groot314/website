3d7aa3c88ed722ca0515923f0d7d4c6ec48415cf
Geoff Root <groot314@gmail.com>
2025-09-08 20:45:41 -0400

    skill block function

--- internal/elements/elements.go
+++ internal/elements/elements.go
@@ -22,3 +22,25 @@ func ConsoleMessages(lines []string) string {
 
 	return result + "</div>"
 }
+
+func SkillBlocks(skills []string) string {
+	result := `
+	  <div class="max-w-3xl mx-auto px-4 py-4">
+	    <h2 class="font-semibold">Skills:</h2>
+	    <div class="flex flex-wrap gap-2">
+		`
+	skillWrap := func(s string) string {
+		start := `
+	      	<p class="block text-center bg-gray-200 dark:bg-gray-700 text-gray-900 dark:text-gray-100 text-lg py-2 px-4 rounded-lg">
+		`
+		end := "</p>"
+
+		return start + s + end
+
+	}
+	for _, skill := range skills {
+		result += skillWrap(skill)
+	}
+
+	return result + "</div>"
+}
