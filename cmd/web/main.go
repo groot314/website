b637004d0b56c5ef8369c036cd1abaf826bc59e6
Geoff Root <groot314@gmail.com>
2025-09-10 19:51:47 -0400

    flags for port and host

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "embed"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,17 +11,19 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-const port = 8080
-
 func main() {
+	port := flag.String("port", "8080", "Port for SSH Server")
+	host := flag.String("host", "localhost", "Host for SSH Server")
+	flag.Parse()
+
 	r := chi.NewRouter()
 
 	r.Mount("/", router.PageRoutes())
 	r.Mount("/ds", router.DataStarRoutes())
 
-	log.Printf("Starting server on http://localhost:%d", port)
+	log.Printf("Starting server on http://%s:%s", *host, *port)
 
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), r); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf("%s:%s", *host, *port), r); err != nil {
 		panic(err)
 	}
 }
